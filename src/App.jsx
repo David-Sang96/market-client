9829c5c3ec7c1e65c43eba7bdf248d5a872974fd
David-Sang96 <luainawl@gmail.com>
2024-02-22 16:12:22 +0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+function App() {
+  const router = createBrowserRouter([]);
+  return <RouterProvider router={router} />;
+}
+
+export default App;
