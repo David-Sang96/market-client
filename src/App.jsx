9d986a51d3a826d7bf020c833e109b25344dc9d7
David-Sang96 <luainawl@gmail.com>
2024-02-22 20:53:10 +0800

    auth done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
-  const router = createBrowserRouter([]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+  ]);
   return <RouterProvider router={router} />;
 }
 
---
+++ src/components/AuthForm.jsx
@@ -0,0 +1,113 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { Form, Input, message } from "antd";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { loginUser, registerUser } from "../apicalls/authApi";
+
+const AuthForm = ({ isLoginPage }) => {
+  const [submitting, setSubmitting] = useState(false);
+
+  const handleOnFinish = async (values) => {
+    setSubmitting(true);
+    if (isLoginPage) {
+      try {
+        const response = await loginUser(values);
+        if (response.isSuccess) {
+          message.success(response.message);
+          localStorage.setItem("token", response.token);
+        } else {
+          throw new Error(response.message);
+        }
+      } catch (err) {
+        message.error(err.message);
+      }
+    } else {
+      try {
+        const response = await registerUser(values);
+        if (response.isSuccess) {
+          message.success(response.message);
+        } else {
+          throw new Error(response.message);
+        }
+      } catch (err) {
+        message.error(err.message);
+      }
+    }
+    setSubmitting(false);
+  };
+
+  return (
+    <section className="flex justify-center w-full mt-28">
+      <div className="px-8 md:px-0 w-[420px]">
+        <h1 className="pb-4 text-3xl font-bold text-blue-600">
+          POINT.IO - {isLoginPage ? "LOGIN" : "REGISTER"}
+        </h1>
+        <Form layout="vertical" onFinish={handleOnFinish}>
+          {isLoginPage || (
+            <Form.Item
+              name="name"
+              label="Name"
+              rules={[
+                { required: true, message: "Please write your username." },
+                { min: 3, message: "Name must have at least 3 characters." },
+              ]}
+              hasFeedback
+            >
+              <Input placeholder="name ..."></Input>
+            </Form.Item>
+          )}
+          <Form.Item
+            name="email"
+            label="Email"
+            rules={[
+              { type: "email", message: "Email is not valid." },
+              { required: true, message: "Please write your email." },
+            ]}
+            hasFeedback
+          >
+            <Input placeholder="email ..."></Input>
+          </Form.Item>
+          <Form.Item
+            name="password"
+            label="Password"
+            rules={[
+              { required: true, message: "Please write your Password." },
+              { min: 5, message: "Password must have at least 5 characters." },
+            ]}
+            hasFeedback
+          >
+            <Input.Password placeholder="password ..."></Input.Password>
+          </Form.Item>
+          <Form.Item>
+            <button
+              className="w-full py-2 text-white bg-blue-600 rounded-md outline-none"
+              disabled={submitting}
+            >
+              {isLoginPage
+                ? `${submitting ? "Logging In..." : "Login"}`
+                : `${submitting ? "Registering..." : "Register"}`}
+            </button>
+          </Form.Item>
+          {isLoginPage ? (
+            <div className="flex items-center justify-between">
+              <p>Don't have account?</p>
+              <Link to={"/register"} className="text-blue-600">
+                Register Here
+              </Link>
+            </div>
+          ) : (
+            <div className="flex items-center justify-between">
+              <p>Already have account?</p>
+              <Link to={"/login"} className="text-blue-600">
+                LogIn Here
+              </Link>
+            </div>
+          )}
+        </Form>
+      </div>
+    </section>
+  );
+};
+
+export default AuthForm;
